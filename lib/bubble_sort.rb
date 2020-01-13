596f20ad53ee8d8d275ef9ca3c6aa6ea84fde40c
Ivan <fivanunam@gmail.com>
2020-01-13 16:43:58 -0600

    run rubocop in bubble_sort.rb file

--- lib/bubble_sort.rb
+++ lib/bubble_sort.rb
@@ -1,43 +1,49 @@
+# frozen_string_literal: true
+
 module BubbleSort
   def bubble_sort(arr)
     return_arr = []
-    return return_arr if !(arr.is_a? Array)
+    return return_arr if arr.class != Array
 
     arr.each_with_index { |item, index| return_arr[index] = item }
-    return return_arr if arr.length() == 1
+    return return_arr if arr.length == 1
 
-    j = return_arr.length() - 1
+    j = return_arr.length - 1
     while j >= 1
       i = 1
       while i <= j
-        return_arr[i],return_arr[i-1] = return_arr[i-1],return_arr[i] if 
-          return_arr[i] < return_arr[i-1]
-
-        i = i + 1
+        if return_arr[i] < return_arr[i - 1]
+          temp = return_arr[i]
+          return_arr[i] = return_arr[i - 1]
+          return_arr[i - 1] = temp
+        end
+        i += 1
       end
-      j = j - 1
+      j -= 1
     end
-    return return_arr
+    return_arr
   end
 
   def bubble_sort_by(arr)
     return_arr = []
-    return return_arr if !(arr.is_a? Array)
+    return return_arr if arr.class != Array
 
     arr.each_with_index { |item, index| return_arr[index] = item }
-    return return_arr if arr.length() == 1
+    return return_arr if arr.length == 1
 
-    j = return_arr.length() - 1
+    j = return_arr.length - 1
     while j >= 1
       i = 1
       while i <= j
-        return_arr[i],return_arr[i-1] = return_arr[i-1],return_arr[i] if 
-          yield(return_arr[i-1],return_arr[i]) > 0
-
-        i = i + 1
+        if yield(return_arr[i - 1], return_arr[i]).positive?
+          temp = return_arr[i]
+          return_arr[i] = return_arr[i - 1]
+          return_arr[i - 1] = temp
+        end
+        i += 1
       end
-      j = j - 1
+      j -= 1
     end
-    return return_arr
+    return_arr
   end
 end
